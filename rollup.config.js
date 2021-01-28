b8de0a2159069b349bfa0cff82f3d9941764291d
urain39 <urain39@qq.com>
2021-01-28 12:44:21 +0800

    添加dist目录。

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
+import { uglify } from 'rollup-plugin-uglify';
+
 export default {
     input: 'index.js',
     output: {
         file: 'dist/jsonrpc.bundle.js',
         format: 'umd',
         name: 'TINYRPC'
-    }
+    },
+    plugins: [
+        uglify()
+    ]
 };
